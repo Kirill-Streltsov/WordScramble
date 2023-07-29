6b6216a25fa8c47ef2ea54c698947f04d4988b2f
Kirill-Streltsov <krlsrlsv@gmail.com>
2023-07-29 11:52:36 +0200

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Kirill Streltsov on 29.07.23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
